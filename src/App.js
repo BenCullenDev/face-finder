37e4e4597866a2500e84bde3693e24163c786075
Ben Cullen <benjaminjcullen1@gmail.com>
2022-04-25 12:23:20 +0100

    added nav component and tachyons css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Navigation from './components/navigation/navigation';
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      {/*<Logo />
+      <LinkForm />
+  <FaceFinder />*/}
     </div>
   );
 }
